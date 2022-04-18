567139432109bb1bb8079bcaeaff85c8a9aa3c45
Jason C. Webb <jwebb@bnl.gov>
2022-04-18 19:54:28 -0400

    Move business logic under a main() function

--- shrek/scripts/buildJobScript.py
+++ shrek/scripts/buildJobScript.py
@@ -24,38 +24,40 @@ handler.addToken( 'JobCommands', buildCodeBlock, None )
 handler.addToken( 'Finalize', buildCodeBlock, None )
 handler.addToken( 'LocalFinalize', buildCodeBlock, None )
 
-#_______________________________________________________________________________________        
+#_______________________________________________________________________________________
 
-parser = argparse.ArgumentParser(description='Build job script from yaml')
-parser.add_argument('yaml', metavar='YAML', type=str, 
+def main():
+
+    parser = argparse.ArgumentParser(description='Build job script from yaml')
+    parser.add_argument('yaml', metavar='YAML', type=str, 
                                         help='input filename')
 
-parser.add_argument('--tag', type=str, help='production tag' )
-args = parser.parse_args()
+    parser.add_argument('--tag', type=str, help='production tag' )
+    args = parser.parse_args()
     
-with open(args.yaml, "r") as stream:
+    with open(args.yaml, "r") as stream:
 
-    definition = yaml.safe_load(stream)
+        definition = yaml.safe_load(stream)
 
-    job = JobDefinition( args.yaml, definition )
+        job = JobDefinition( args.yaml, definition )
     
-    handler.traverse( definition )
+        handler.traverse( definition )
 
-    job.parameters  = handler.result('Parameters')
-    job.inputs      = handler.result('InputDataSets')
-    job.outputs     = handler.result('OutputDataSets')
-    job.secondaries = handler.result('SecondaryDataSets')
-    job.localinit   = handler.result('LocalInit')
-    job.init        = handler.result('Initialize')
-    job.commands    = handler.result('JobCommands')
-    job.finish      = handler.result('Finalize')
-    job.localfinish = handler.result('LocalFinalize')
+        job.parameters  = handler.result('Parameters')
+        job.inputs      = handler.result('InputDataSets')
+        job.outputs     = handler.result('OutputDataSets')
+        job.secondaries = handler.result('SecondaryDataSets')
+        job.localinit   = handler.result('LocalInit')
+        job.init        = handler.result('Initialize')
+        job.commands    = handler.result('JobCommands')
+        job.finish      = handler.result('Finalize')
+        job.localfinish = handler.result('LocalFinalize')
 
-    #
-    # Convention is that the PanDA will pass certain parameters
-    # through the command line
-    #
-    print( \
+        #
+        # Convention is that the PanDA will pass certain parameters
+        # through the command line
+        #
+        print( \
 """
 
 # Script command line parameters
@@ -73,38 +75,43 @@ fi
 """
 )
 
-    #
-    # Build and output environment block
-    #
-    print(job.parameters.params)
+        #
+        # Build and output environment block
+        #
+        print(job.parameters.params)
 
-    #
-    # Data set names
-    #
-    for (i,ds) in enumerate(job.inputs):
-        print("# Input DS %s [%i/%i]"%(ds.name,i+1,len(job.inputs)))
-        print("export inputDS%i_name=%s"%(i+1,ds.name) )
+        #
+        # Data set names
+        #
+        for (i,ds) in enumerate(job.inputs):
+            print("# Input DS %s [%i/%i]"%(ds.name,i+1,len(job.inputs)))
+            print("export inputDS%i_name=%s"%(i+1,ds.name) )
 
-    print("")    
+        print("")    
 
-    for (i,ds) in enumerate(job.secondaries):
-        print("# Secondary DS %s [%i/%i]"%(ds.name,i+1,len(job.inputs)))
-        print("export secondaryDS%i_name=%s"%(i+1,ds.name) )    
+        for (i,ds) in enumerate(job.secondaries):
+            print("# Secondary DS %s [%i/%i]"%(ds.name,i+1,len(job.inputs)))
+            print("export secondaryDS%i_name=%s"%(i+1,ds.name) )    
 
-    #
-    # Worker node initialization
-    #
-    if job.init.block:
-        print( job.init.block )
+            #
+            # Worker node initialization
+            #
+        if job.init.block:
+            print( job.init.block )
 
-    #
-    # Worker node execution script
-    #
-    if job.commands.block:
-        print( job.commands.block )
+        #
+        # Worker node execution script
+        #
+        if job.commands.block:
+            print( job.commands.block )
 
-    #
-    # Worker node finalization
-    #
-    if job.finish.block:
-        print( job.finish.block )
+        #
+        # Worker node finalization
+        #
+        if job.finish.block:
+            print( job.finish.block )
+
+
+
+if __name__ == '__main__':
+    main()
