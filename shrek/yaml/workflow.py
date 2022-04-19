dce7abd5e73b34348fbcb22b606983182a5da9ac
Jason C. Webb <jwebb@bnl.gov>
2022-04-18 20:28:41 -0400

    Jobs are added to collections keyed by their input and output data sets

--- shrek/yaml/workflow.py
+++ shrek/yaml/workflow.py
@@ -1,9 +1,25 @@
 import networks
 from shrek.yaml.config import Config
+from collections import defaultdict
 
 class Workflow:
     def __init__(self):
-        jobs  = [] # list of jobs (nodes)
-        outin = [] # pairs of jobs connected output --> input (edges)
+        self.jobs     = []                # list of jobs (nodes)
+        self.jobs_in  = defaultdict(list) # list of jobs keyed by input       
+        self.jobs_out = defaultdict(list) # list of jobs keyed by output
+        self.inputs   = {}                # keys contain list of all inputs       
+        self.outputs  = {}                # keys contain list of all outputs
+
+    def addJob(self,job):
+        self.jobs.append(job)
+
+        for inp in job.inputs:
+            self.jobs_in[inp.name].append(job)
+            self.inputs[inp.name]=1
+
+        for out in job.outputs:
+            self.jobs_out[out.name].append(job)
+            self.outputs[out.name]=1
+        
 
         
