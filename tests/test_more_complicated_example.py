d5166c5f4acde65334591df1ee1275d3a283ceea
Jason C. Webb <jwebb@bnl.gov>
2022-04-22 14:38:09 -0400

    Input field should specify signal and background

--- tests/test_more_complicated_example.py
+++ tests/test_more_complicated_example.py
@@ -1,6 +1,7 @@
 from pykwalify.core import Core
 import os
 import yaml
+import pprint
 
 from shrek.scripts.buildJobScript import buildJobDefinition
 from shrek.scripts.buildWorkflowGraph import buildWorkflowGraph
@@ -26,5 +27,51 @@ def test_create_the_workflow():
 
 def test_we_should_be_able_to_parse_the_workflow():
     wf = commonWorkflow()
-    print(wf)
-    yaml.safe_load( str(wf) )
+    dict_ = yaml.safe_load( str(wf) )
+    #pprint.pprint(dict_)
+
+def test_workflow_has_expected_fields():
+    wf = commonWorkflow()
+    dict_ = yaml.safe_load( str(wf) )
+    fields = [ "inputs",
+               "outputs",
+               "requirements",
+               "steps",
+               "class",
+               "cwlVersion"
+               ]
+    for f in fields:
+        x = dict_[f]
+
+
+def test_workflow_inputs():
+    wf = commonWorkflow()
+    dict_ = yaml.safe_load( str(wf) )
+    inputs = dict_["inputs"]
+    make_signal = inputs['make_signal_input']
+    assert( make_signal['type'] == "string" )
+    assert( make_signal['default'] == "signal" )    
+    
+    make_background_1 = inputs["make_background_1_input"]
+    assert(make_background_1['type'] == "string")
+    assert(make_background_1['default'] == "background")    
+
+    
+
+def test_workflow_outputs():
+    wf = commonWorkflow()
+    dict_ = yaml.safe_load( str(wf) )
+    outputs = dict_["outputs"]
+    pprint.pprint(outputs)
+
+    
+def test_workflow_class():
+    wf = commonWorkflow()
+    dict_ = yaml.safe_load( str(wf) )
+    class_ = dict_["class"]
+    assert(class_ == 'Workflow')
+
+    
+
+
+    
