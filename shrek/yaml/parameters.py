581346a3297f0bf31b126e20672e1e8cdf7a681e
Jason C. Webb <jwebb@bnl.gov>
2022-04-28 16:51:04 -0400

    Remove newlines from parameter inputs

--- shrek/yaml/parameters.py
+++ shrek/yaml/parameters.py
@@ -5,6 +5,8 @@ class ParameterBlock:
 def buildParameterBlock(key,params):
     output=""
     for (k,v) in params.items():
+        v = str(v)
+        v = ''.join(v.splitlines())
         output = output + "export %s=%s\n"%(k,str(v))
     p = ParameterBlock()
     p.params = output
