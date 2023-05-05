e979f5b9bc20329a27555beb4f6f89e7479e4e1f
klendathu2k <jwebb@bnl.gov>
2023-05-05 14:50:23 -0400

    [plugins] Source the rucio setup

--- plugins/rucio-functions.sh
+++ plugins/rucio-functions.sh
@@ -1,3 +1,6 @@
+
+source /cvmfs/sphenix.sdcc.bnl.gov/rucio-clients/setup.sh
+
 # Simplistic function to get the list of files (pfns) contained in a dataset.
 # ... does not required that the list of files is unique
 function get_file_list() {
