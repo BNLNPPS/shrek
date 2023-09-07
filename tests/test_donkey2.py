d7d0750ed8d95b524f2a400475715292bd29d1ed
klendathu2k <jwebb@bnl.gov>
2023-09-07 08:46:02 -0400

    [donkey2] Add test for the removal of a dataset

--- tests/test_donkey2.py
+++ tests/test_donkey2.py
@@ -84,6 +84,61 @@ def test_donkey2_add_pop_dataset_collection():
         with pytest.raises(IndexError):
             co.pop( n )
 
+def test_donkey2_add_remove_dataset_collection():
+
+    from donkey.dataset import dataset_collection as collection
+    from donkey.dataset import dataset
+    
+    d1 = dataset()
+    d1.name      = "dataset1"
+    d1.runnumber = 12345
+    d1.event     = "test_add"
+    d1.created   = str(datetime.datetime.utcnow())
+
+    d2 = dataset()
+    d2.name      = "dataset2"
+    d2.runnumber = 12345
+    d2.event     = "test_add"
+    d2.created   = str(datetime.datetime.utcnow())
+
+    d3 = dataset()
+    d3.name      = "dataset3"
+    d3.runnumber = 12345
+    d3.event     = "test_add"
+    d3.created   = str(datetime.datetime.utcnow())
+
+    coll = collection( "test_donkey2_add_remove_dataset_collection" )
+    coll.addlist('new', 'addition/removal test')
+
+    coll.add('new',d1)
+    coll.add('new',d2)
+    coll.add('new',d3)
+
+    assert coll.exists('new',d1), 'Dataset 1 exists after addition'
+    assert coll.exists('new',d2), 'Dataset 2 exists after addition'
+    assert coll.exists('new',d3), 'Dataset 3 exists after addition'
+
+    coll.rem('new',d1)
+    assert not coll.exists('new',d1), 'Dataset 1 does not exist after remove'
+    assert coll.exists('new',d2), 'Dataset 2 still exists after ds1 removal'
+    assert coll.exists('new',d3), 'Dataset 3 still exists after ds1 removal'    
+
+def test_donkey2_add_to_unkown_list():
+
+    from donkey.dataset import dataset_collection as collection
+    from donkey.dataset import dataset
+    
+    ds = dataset()
+    ds.name      = "dataset1"
+    ds.runnumber = 12345
+    ds.event     = "test_add"
+    ds.created   = str(datetime.datetime.utcnow())
+
+    co = collection( "test_donkey2_add_to_unknown_list", "new", "Newly created datasets" )
+
+    with pytest.raises(KeyError):  # This will bomb
+        co.add( 'old', ds )
+    
 def test_donkey2_multiadd_dataset_collection():
 
     from donkey.dataset import dataset_collection as collection
